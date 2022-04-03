7b0723735daff1e40e286484ad4e1d0e27caf519
jcelisca <celiss1997@gmail.com>
2022-04-03 08:39:51 -0500

    Se crea una vista para el perfil de usuario

---
+++ src/containers/Profile.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { getDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase/credentials";
+
+const Profile = () => {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+            if (docSnap.exists) {
+                setUser(docSnap.data());
+            }
+        });
+    }, []);
+
+    return user ? (
+        <section>
+            <h1>Wellcome</h1>
+            <div className="text_container">
+                <h3>{user.name}</h3>
+                <p>{user.email}</p>
+                <hr />
+                <small>Joined on: {user.createdAt.toDate().toDateString()}</small>
+            </div>
+        </section>
+    ) : (
+        <h1>Loading...</h1>
+    )
+
+}
+
+export default Profile;
