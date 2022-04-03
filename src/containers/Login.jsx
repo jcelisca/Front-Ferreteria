cdd7e5b104fea9fefa6e372910c3f88e4d9e8b74
jcelisca <celiss1997@gmail.com>
2022-04-03 08:36:28 -0500

    Se configura correctamente el Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,29 +1,106 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { auth, db } from "../firebase/credentials";
+import { updateDoc, doc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
-  return (
-      <div className="abs-center">
-      <form className="form-login"  >
-          <h1>Login</h1>
-          <div className="col-md-6">
-              <input type="email" className="form-control" id="validationCustom01" placeholder="Email" required/>
-                  <div className="invalid-feedback">
-                      Please provide a valid email.
-                  </div>
-          </div>
-          <div className="col-md-6">
-              <br/>
-              <input type="password" className="form-control" id="validationCustom02" placeholder="Password" required/>
-                  <div className="invalid-feedback">
-                      Please provide a valid password.
-                  </div>
-          </div>
-          <div className="col-12">
-              <br/>
-              <button className="btn btn-primary" type="submit">Sign in</button>
-          </div>
-      </form>
-      </div>
-  );
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        loading: false,
+    });
+
+    const history = useNavigate();
+    const {email, password, loading } = data;
+
+    const handleChange = (e)=>{
+        setData({...data, [e.target.name]: e.target.value});
+    };
+
+    const signInWithGoogle = async e=>{
+        e.preventDefault();
+        try{
+            const provider = new GoogleAuthProvider();
+            await signInWithPopup(auth, provider);
+            history("/");
+        }catch(err){
+            setData({ ...data, loading: false });
+        }
+    }
+
+    const signInWithGithub = async e=>{
+        e.preventDefault();
+        try{
+            const provider = new GithubAuthProvider();
+            await signInWithPopup(auth, provider);
+            history("/");
+        }catch(err){
+            setData({ ...data, loading: false });
+        }
+    }
+
+    const handleSubmit = async e =>{
+        e.preventDefault();
+        setData({ ...data, loading: true })
+        try{
+            const result = await signInWithEmailAndPassword(auth, email, password);
+            await updateDoc(doc(db, 'users', result.user.uid),{
+                isOnline: true,
+            });
+            setData({
+                email: "",
+                password: "",
+                loading: false,
+            });
+            history("/");
+        }catch(err){
+            setData({ ...data, loading: false });
+            alert("Usuario o contraseÃ±a incorrecta");
+        }
+    }
+
+    return (
+        <section>
+        <form className="form-login" onSubmit={handleSubmit} >
+            <h2>Sign In to Chatty</h2>
+            <br/>
+            <br/>
+            <div className="col-md-10">
+                <input type="email" className="form-control" name="email" placeholder="Email" value={email} onChange={handleChange} required/>
+                    <div className="invalid-feedback">
+                        Please provide a valid email.
+                    </div>
+            </div>
+            <div className="col-md-10">
+                <br/>
+                <input type="password" className="form-control" name="password" placeholder="Password" value={password} onChange={handleChange} minLength="6" required/>
+                    <div className="invalid-feedback">
+                        Please provide a valid password.
+                    </div>
+            </div>
+            <div className="col-12">
+                <br/>
+                <button className="btn btn-primary" type="submit" disabled={loading}>
+                    {loading ? "Creating ..." : "Sign In"}
+                </button>
+            </div>
+            <div className="col-20">
+                <br/>
+                <button className="btn-google" type="submit" onClick={signInWithGoogle}>
+                    Sign In with Google
+                </button>
+            </div>
+            <div className="col-20">
+                <br/>
+                <button className="btn-github" type="submit" onClick={signInWithGithub}>
+                    Sign In with Github
+                </button>
+            </div>
+        </form>
+        </section>
+     );
 }
 
 export default Login;
