3550a6cea4c1648afd7fd981d9172c11abb19dc1
jcelisca <celiss1997@gmail.com>
2022-04-03 08:57:40 -0500

    Se configuran las rutas de acceso

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from "./containers/Register";
+import Dashboard from "./containers/Dashboard";
+import Login from "./containers/Login";
+import Profile from './containers/Profile';
+import Navbar from './containers/Navbar';
+import AuthProvider from './context/auth';
+import Inventory from "./containers/Inventory"
+
 
 function App() {
   return (
-    <div className="div-welcome">
-      <Register/>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/inventory" element={<Inventory />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
